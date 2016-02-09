c2820068c03bfd98e325b41d85499847d525ddbf
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2016-02-09 01:34:37 +0000

    [tests/tgeneric_ui.c] Added tests of the extreme integer values, so that tsi_op fails with a runtime error in si_op.c when using an UB sanitizer (bug fixed in r9999 in the trunk). (merged changeset r10001 from the trunk)

---
+++ tests/tversion.c
@@ -0,0 +1,104 @@
+/* Test file for mpfr_version.
+
+Copyright 2004-2016 Free Software Foundation, Inc.
+Contributed by the AriC and Caramba projects, INRIA.
+
+This file is part of the GNU MPFR Library.
+
+The GNU MPFR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MPFR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
+http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
+
+#include <stdlib.h>
+
+#include "mpfr-test.h"
+
+int
+main (void)
+{
+  int err = 0;
+
+  /* Test the GMP and MPFR versions. */
+  if (test_version ())
+    exit (1);
+
+  printf ("[tversion] MPFR %s\n", MPFR_VERSION_STRING);
+
+#ifdef __GNUC__
+  printf ("[tversion] GCC: %d.%d.%d\n",
+          __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+#endif
+#ifdef __MPIR_VERSION
+  printf ("[tversion] MPIR: header %d.%d.%d, library %s\n",
+          __MPIR_VERSION, __MPIR_VERSION_MINOR, __MPIR_VERSION_PATCHLEVEL,
+          mpir_version);
+#else
+  printf ("[tversion] GMP: header %d.%d.%d, library %s\n",
+          __GNU_MP_VERSION, __GNU_MP_VERSION_MINOR, __GNU_MP_VERSION_PATCHLEVEL,
+          gmp_version);
+#endif
+
+  if (
+#ifdef MPFR_USE_THREAD_SAFE
+      !
+#endif
+      mpfr_buildopt_tls_p ())
+    {
+      printf ("ERROR! mpfr_buildopt_tls_p() and macros"
+              " do not match!\n");
+      err = 1;
+    }
+
+  if (
+#ifdef MPFR_WANT_DECIMAL_FLOATS
+      !
+#endif
+      mpfr_buildopt_decimal_p ())
+    {
+      printf ("ERROR! mpfr_buildopt_decimal_p() and macros"
+              " do not match!\n");
+      err = 1;
+    }
+
+  if (
+#if defined(MPFR_HAVE_GMP_IMPL) || defined(WANT_GMP_INTERNALS)
+      !
+#endif
+      mpfr_buildopt_gmpinternals_p ())
+    {
+      printf ("ERROR! mpfr_buildopt_gmpinternals_p() and macros"
+              " do not match!\n");
+      err = 1;
+    }
+
+  printf ("[tversion] TLS = %s, decimal = %s, GMP internals = %s\n",
+          mpfr_buildopt_tls_p () ? "yes" : "no",
+          mpfr_buildopt_decimal_p () ? "yes" : "no",
+          mpfr_buildopt_gmpinternals_p () ? "yes" : "no");
+
+  if (strcmp (mpfr_buildopt_tune_case (), MPFR_TUNE_CASE) != 0)
+    {
+      printf ("ERROR! mpfr_buildopt_tune_case() and MPFR_TUNE_CASE"
+              " do not match!\n  %s\n  %s\n",
+              mpfr_buildopt_tune_case (), MPFR_TUNE_CASE);
+      err = 1;
+    }
+  else
+    printf ("[tversion] MPFR tuning parameters from %s\n", MPFR_TUNE_CASE);
+
+  if (strcmp (mpfr_get_patches (), "") != 0)
+    printf ("[tversion] MPFR patches: %s\n", mpfr_get_patches ());
+
+  return err;
+}
