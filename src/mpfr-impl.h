ryepdx/mpfr

commit 4b5a70f9c72656cfd2ecd104e1eb42f6f8928cc4

Added shared cache support (from a patch by Patrick Pélissier).
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@10327 280ebfd0-de03-0410-8827-d642c229c3f4

src/mpfr-impl.h
@@ -78,6 +78,9 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 /* For the definition of MPFR_THREAD_ATTR. GCC/ICC detection macros are
    no longer used, as they sometimes gave incorrect information about
    the support of thread-local variables. A configure check is now done. */
+#if defined (WANT_SHARED_CACHE)
+# define MPFR_NEED_THREAD_LOCK 1
+#endif
 #include "mpfr-thread.h"
 
 #ifdef MPFR_HAVE_GMP_IMPL /* Build with gmp internals */
@@ -199,11 +202,18 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 extern "C" {
 #endif
 
-/* Cache struct */
+#if defined(WANT_SHARED_CACHE)
+# define MPFR_CACHE_ATTR
+#else
+# define MPFR_CACHE_ATTR MPFR_THREAD_ATTR
+#endif
+
 struct __gmpfr_cache_s {
   mpfr_t x;
   int inexact;
   int (*func)(mpfr_ptr, mpfr_rnd_t);
+  MPFR_DEFERRED_INIT_SLAVE_DECL()
+  MPFR_LOCK_DECL(lock)
 };
 typedef struct __gmpfr_cache_s mpfr_cache_t[1];
 typedef struct __gmpfr_cache_s *mpfr_cache_ptr;
@@ -218,22 +228,22 @@ extern MPFR_THREAD_ATTR mpfr_exp_t   __gmpfr_emin;
 extern MPFR_THREAD_ATTR mpfr_exp_t   __gmpfr_emax;
 extern MPFR_THREAD_ATTR mpfr_prec_t  __gmpfr_default_fp_bit_precision;
 extern MPFR_THREAD_ATTR mpfr_rnd_t   __gmpfr_default_rounding_mode;
-extern MPFR_THREAD_ATTR mpfr_cache_t __gmpfr_cache_const_euler;
-extern MPFR_THREAD_ATTR mpfr_cache_t __gmpfr_cache_const_catalan;
+extern MPFR_CACHE_ATTR  mpfr_cache_t __gmpfr_cache_const_euler;
+extern MPFR_CACHE_ATTR  mpfr_cache_t __gmpfr_cache_const_catalan;
 # ifndef MPFR_USE_LOGGING
-extern MPFR_THREAD_ATTR mpfr_cache_t __gmpfr_cache_const_pi;
-extern MPFR_THREAD_ATTR mpfr_cache_t __gmpfr_cache_const_log2;
+extern MPFR_CACHE_ATTR  mpfr_cache_t __gmpfr_cache_const_pi;
+extern MPFR_CACHE_ATTR  mpfr_cache_t __gmpfr_cache_const_log2;
 # else
 /* Two constants are used by the logging functions (via mpfr_fprintf,
    then mpfr_log, for the base conversion): pi and log(2). Since the
    mpfr_cache function isn't re-entrant when working on the same cache,
    we need to define two caches for each constant. */
-extern MPFR_THREAD_ATTR mpfr_cache_t   __gmpfr_normal_pi;
-extern MPFR_THREAD_ATTR mpfr_cache_t   __gmpfr_normal_log2;
-extern MPFR_THREAD_ATTR mpfr_cache_t   __gmpfr_logging_pi;
-extern MPFR_THREAD_ATTR mpfr_cache_t   __gmpfr_logging_log2;
-extern MPFR_THREAD_ATTR mpfr_cache_ptr __gmpfr_cache_const_pi;
-extern MPFR_THREAD_ATTR mpfr_cache_ptr __gmpfr_cache_const_log2;
+extern MPFR_CACHE_ATTR  mpfr_cache_t   __gmpfr_normal_pi;
+extern MPFR_CACHE_ATTR  mpfr_cache_t   __gmpfr_normal_log2;
+extern MPFR_CACHE_ATTR  mpfr_cache_t   __gmpfr_logging_pi;
+extern MPFR_CACHE_ATTR  mpfr_cache_t   __gmpfr_logging_log2;
+extern MPFR_CACHE_ATTR  mpfr_cache_ptr __gmpfr_cache_const_pi;
+extern MPFR_CACHE_ATTR  mpfr_cache_ptr __gmpfr_cache_const_log2;
 # endif
 #endif
 
@@ -1097,15 +1107,24 @@ typedef union { mp_size_t s; mp_limb_t l; } mpfr_size_limb_t;
  *******************  Cache macros  *******************
  ******************************************************/
 
+/* Cache struct */
 #define mpfr_const_pi(_d,_r)    mpfr_cache(_d, __gmpfr_cache_const_pi,_r)
 #define mpfr_const_log2(_d,_r)  mpfr_cache(_d, __gmpfr_cache_const_log2, _r)
 #define mpfr_const_euler(_d,_r) mpfr_cache(_d, __gmpfr_cache_const_euler, _r)
 #define mpfr_const_catalan(_d,_r) mpfr_cache(_d,__gmpfr_cache_const_catalan,_r)
 
+/* Declare a global cache for a MPFR constant.
+   If the shared cache is enables, and if the constructor/destructor
+   attributes are available, we need to initialize the shared lock of
+   the cache with a constructor. It is the goal of the macro
+   MPFR_DEFERRED_INIT_MASTER_DECL */
 #define MPFR_DECL_INIT_CACHE(_cache,_func)                           \
-  MPFR_THREAD_ATTR mpfr_cache_t _cache =                             \
-    {{{{0,MPFR_SIGN_POS,0,(mp_limb_t*)0}},0,_func}}
-
+  MPFR_DEFERRED_INIT_MASTER_DECL(_func,                              \
+                                 MPFR_LOCK_INIT( (_cache)->lock),    \
+                                 MPFR_LOCK_CLEAR((_cache)->lock))    \
+  MPFR_CACHE_ATTR mpfr_cache_t _cache =                              \
+    {{{{0,MPFR_SIGN_POS,0,(mp_limb_t*)0}},0,_func                    \
+      MPFR_DEFERRED_INIT_SLAVE_VALUE(_func) }};
 
 /******************************************************
  ***************  Threshold parameters  ***************

TODO
@@ -370,10 +370,6 @@ Table of contents:
   Mulders short product was slower than a full product for large sizes.
   This should be precisely analyzed and fixed if needed.
 
-- implement optional cache sharing between threads (with light locking).
-  https://sympa.inria.fr/sympa/arc/mpfr/2013-09/msg00000.html
-  https://sympa.inria.fr/sympa/arc/mpfr/2013-09/msg00002.html
-
 - for various functions, check the timings as a function of the magnitude
   of the input (and the input and/or output precisions?), and use better
   thresholds for asymptotic expansions.

acinclude.m4
@@ -206,6 +206,15 @@ if test "$mpfr_cv_have_builtin_unreachable" = "yes"; then
    [Define if the __builtin_unreachable GCC built-in is supported.])
 fi
 
+dnl Check for attribute constructor and destructor
+MPFR_CHECK_CONSTRUCTOR_ATTR()
+
+dnl Check for POSIX Thread.
+ACX_PTHREAD()
+
+dnl Check for ISO C11 Thread
+MPFR_CHECK_C11_THREAD()
+
 dnl Check for fesetround
 AC_CACHE_CHECK([for fesetround], mpfr_cv_have_fesetround, [
 saved_LIBS="$LIBS"
@@ -498,6 +507,7 @@ int main (void) {
       AC_DEFINE([MPFR_USE_THREAD_SAFE],1,[Build MPFR as thread safe])
       AC_DEFINE([MPFR_USE_C11_THREAD_SAFE],1,[Build MPFR as thread safe using C11])
       tls_c11_support=yes
+      enable_thread_safe=yes
      ],
      [AC_MSG_RESULT(no)
      ],
@@ -521,6 +531,7 @@ then
    ]])],
       [AC_MSG_RESULT(yes)
        AC_DEFINE([MPFR_USE_THREAD_SAFE],1,[Build MPFR as thread safe])
+       enable_thread_safe=yes
       ],
       [AC_MSG_RESULT(no)
        if test "$enable_thread_safe" = yes; then
@@ -618,6 +629,12 @@ int main (void) {
      [AC_MSG_RESULT([cannot test, assume no])
      ])
 CPPFLAGS="$saved_CPPFLAGS"
+
+
+dnl Check if the shared cache was requested and its requirements are ok.
+if test "$mpfr_want_shared_cache" = yes ;then
+   MPFR_CHECK_SHARED_CACHE()
+fi
 
 ])
 dnl end of MPFR_CONFIGS
@@ -1220,7 +1237,6 @@ esac
 dnl  MPFR_CHECK_LIBQUADMATH
 dnl  ---------------
 dnl  Determine a math library -lquadmath to use.
-
 AC_DEFUN([MPFR_CHECK_LIBQUADMATH],
 [AC_REQUIRE([AC_CANONICAL_HOST])
 AC_SUBST(MPFR_LIBQUADMATH,'')
@@ -1347,4 +1363,100 @@ MPFR_FUNC_GMP_PRINTF_SPEC([td], [ptrdiff_t], [
     ],
     [AC_DEFINE([PRINTF_T], 1, [gmp_printf can read ptrdiff_t])],
     [AC_DEFINE([NPRINTF_T], 1, [gmp_printf cannot read ptrdiff_t])])
+])
+
+
+dnl MPFR_CHECK_SHARED_CACHE
+dnl ----------------------
+dnl Check if the conditions for the shared cache are met:
+dnl  * either pthread / C11 are available.
+dnl  * either constructor or once.
+AC_DEFUN([MPFR_CHECK_SHARED_CACHE], [
+  AC_MSG_CHECKING(if shared cache is supported)
+  if test "$enable_logging" = yes ; then
+    AC_MSG_RESULT(no)
+    AC_MSG_ERROR([shared cache does not work with logging support.])
+dnl because logging support disables threading support
+  elif test "$enable_thread_safe" != yes ; then
+    AC_MSG_RESULT(no)
+    AC_MSG_ERROR([shared cache needs thread attribute.])
+  elif test "$ax_pthread_ok" != yes && "$mpfr_c11_thread_ok" != yes ; then
+    AC_MSG_RESULT(no)
+    AC_MSG_ERROR([shared cache needs pthread/C11 library.])
+  else
+    AC_MSG_RESULT(yes)
+    if test "$ax_pthread_ok" = yes ; then
+        CFLAGS="$CFLAGS $PTHREAD_CFLAGS"
+        LIBS="$LIBS $PTHREAD_LIBS"
+    fi
+  fi
+])
+
+dnl MPFR_CHECK_CONSTRUCTOR_ATTR
+dnl ---------------------------
+dnl Check for constructor/destructor attributes to function.
+dnl Output: Define
+dnl  * MPFR_HAVE_CONSTRUCTOR_ATTR C define
+dnl  * mpfr_have_constructor_destructor_attributes shell variable to yes
+dnl if supported.
+AC_DEFUN([MPFR_CHECK_CONSTRUCTOR_ATTR], [
+AC_MSG_CHECKING([for constructor and destructor attributes])
+AC_LINK_IFELSE([AC_LANG_PROGRAM([[
+#include <stdlib.h>
+int x = 0;
+__attribute__((constructor)) static void
+call_f(void) { x = 1742; }
+__attribute__((destructor)) static void
+call_g(void) { x = 1448; }
+]], [[
+    return (x == 1742) ? 0 : 1;
+]])], [AC_MSG_RESULT(yes)
+       mpfr_have_constructor_destructor_attributes=yes ],
+      [AC_MSG_RESULT(no)]
+)
+
+if test "$mpfr_have_constructor_destructor_attributes" = "yes"; then
+  AC_DEFINE(MPFR_HAVE_CONSTRUCTOR_ATTR, 1,
+   [Define if the constructor/destructor GCC attributes are supported.])
+fi
+])
+
+dnl MPFR_CHECK_C11_THREAD
+dnl ---------------------
+dnl Check for C11 thread support
+dnl Output: Define
+dnl  * MPFR_HAVE_C11_LOCK C define
+dnl  * mpfr_c11_thread_ok shell variable to yes
+dnl if supported.
+dnl We don't check for __STDC_NO_THREADS__ define variable but rather try to mimic our usage.
+AC_DEFUN([MPFR_CHECK_C11_THREAD], [
+AC_MSG_CHECKING([for ISO C11 thread support])
+AC_LINK_IFELSE([AC_LANG_PROGRAM([[
+#include <assert.h>
+#include <thread.h>
+ mtx_t lock;
+ once_flag once = ONCE_FLAG_INIT;
+ thrd_t thd_idx;
+ int x = 0;
+ void once_call (void) { x = 1; }
+]], [[
+    int err;
+    err = mtx_init(&lock, mtx_plain);
+    assert(err == thrd_success);
+    err = mtx_lock(&lock);
+    assert(err == thrd_success);
+    err = mtx_unlock(&lock);
+    assert(err == thrd_success);
+    mtx_destroy(&lock);
+    once_call(&once, once_call);
+    return x == 1 ? 0 : -1;
+]])], [AC_MSG_RESULT(yes)
+       mpfr_c11_thread_ok=yes ],
+      [AC_MSG_RESULT(no)]
+)
+
+if test "$mpfr_c11_thread_ok" = "yes"; then
+  AC_DEFINE(MPFR_HAVE_C11_LOCK, 1,
+   [Define if the ISO C11 Thread is supported.])
+fi
 ])

configure.ac
@@ -167,6 +167,17 @@ AC_ARG_ENABLE(thread-safe,
       *)   AC_MSG_ERROR([bad value for --enable-thread-safe: yes or no]) ;;
      esac])
 
+AC_ARG_ENABLE(shared-cache,
+   [  --enable-shared-cache   enable use of caches shared by all threads,
+                          for all MPFR constants.  It usually makes MPFR
+                          dependent on PTHREAD [[default=no]]],
+   [ case $enableval in
+      yes) mpfr_want_shared_cache=yes
+         AC_DEFINE([WANT_SHARED_CACHE],1,[Want shared cache]) ;;
+      no)  ;;
+      *) AC_MSG_ERROR([bad value for --enable-shared-cache: yes or no]) ;;
+     esac])
+
 AC_ARG_ENABLE(warnings,
    [  --enable-warnings       allow MPFR to output warnings to stderr [[default=no]]],
    [ case $enableval in

doc/README.dev
@@ -24,13 +24,14 @@ Notes for the MPFR developers and Subversion users
 
 To compile source code obtained from the Subversion repository, you
 need some GNU development utilities: aclocal, autoheader, automake,
-autoconf 2.60 (at least) and libtoolize. As some files like "configure"
-are not part of the Subversion repository, you first need to run
-"autoreconf -i" (or autogen.sh, which could be used later to update
-the config files). Then you can run "configure" in the usual way
-(see the INSTALL file, but note that there are no patches to apply,
-and the URLs are not valid since the corresponding version has not
-been released yet).
+autoconf 2.60 (at least), libtoolize, and the AX_PTHREAD macro from
+<http://www.gnu.org/software/autoconf-archive/ax_pthread.html>.
+As some files like "configure" are not part of the Subversion
+repository, you first need to run "autoreconf -i" (or autogen.sh,
+which could be used later to update the config files). Then you can
+run "configure" in the usual way (see the INSTALL file, but note that
+there are no patches to apply, and the URLs are not valid since the
+corresponding version has not been released yet).
 
 To generate mpfr.info, you need texinfo version 4.2 (or higher).
 

doc/mpfr.texi
@@ -2246,12 +2246,18 @@ use @code{mpfr_free_cache}.
 @end deftypefun
 
 @deftypefun void mpfr_free_cache (void)
-Free various caches used by MPFR internally, in particular the
-caches used by the functions computing constants (@code{mpfr_const_log2},
-@code{mpfr_const_pi},
-@code{mpfr_const_euler} and @code{mpfr_const_catalan}).
+Free various caches used by MPFR internally, which are local to the
+current thread.
 You should call this function before terminating a thread, even if you did
 not call these functions directly (they could have been called internally).
+@end deftypefun
+
+@deftypefun void mpfr_free_cache2 (mpfr_free_cache_t @var{way})
+Free various caches used by MPFR internally,
+either the caches local to the current thread (if @var{way} is
+@code{MPFR_FREE_LOCAL_CACHE}
+or the caches common to all threads (if @var{way} is
+@code{MPFR_FREE_GLOBAL_CACHE}.
 @end deftypefun
 
 @deftypefun int mpfr_sum (mpfr_t @var{rop}, mpfr_ptr const @var{tab}[], unsigned long int @var{n}, mpfr_rnd_t @var{rnd})
@@ -3112,6 +3118,15 @@ return zero otherwise.
 Return a non-zero value if MPFR was compiled with GMP internals
 (that is, MPFR was built with either @code{--with-gmp-build} or
 @code{--enable-gmp-internals} configure option), return zero otherwise.
+@end deftypefun
+
+@deftypefun int mpfr_buildopt_sharedcache_p (void)
+Return a non-zero value if MPFR was compiled so that all threads share the same
+cache for one MPFR constant - like @code{mpfr_const_pi} or @code{mpfr_const_log2}
+(that is, MPFR was built with the @code{--enable-shared-cache} configure option),
+return zero otherwise.
+If the return value is non-zero, MPFR applications may need to be compiled with
+@code{-pthread} option.
 @end deftypefun
 
 @deftypefun {const char *} mpfr_buildopt_tune_case (void)

src/buildopt.c
@@ -63,6 +63,16 @@ mpfr_buildopt_gmpinternals_p (void)
 #endif
 }
 
+int
+mpfr_buildopt_sharedcache_p (void)
+{
+#if defined (WANT_SHARED_CACHE)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
 const char *mpfr_buildopt_tune_case (void)
 {
   /* MPFR_TUNE_CASE is always defined (can be "default"). */

src/cache.c
@@ -39,8 +39,17 @@ mpfr_clear_cache (mpfr_cache_t cache)
 {
   if (MPFR_UNLIKELY (MPFR_PREC (cache->x) != 0))
     {
-      mpfr_clear (cache->x);
-      MPFR_PREC (cache->x) = 0;
+      /* Get the cache in read write mode */
+      MPFR_LOCK_WRITE(cache->lock);
+
+      if (MPFR_LIKELY (MPFR_PREC (cache->x) != 0))
+        {
+          mpfr_clear (cache->x);
+          MPFR_PREC (cache->x) = 0;
+        }
+
+      /* Free the cache in read write mode */
+      MPFR_UNLOCK_WRITE(cache->lock);
     }
 }
 
@@ -48,32 +57,54 @@ int
 mpfr_cache (mpfr_ptr dest, mpfr_cache_t cache, mpfr_rnd_t rnd)
 {
   mpfr_prec_t prec = MPFR_PREC (dest);
-  mpfr_prec_t pold = MPFR_PREC (cache->x);
+  mpfr_prec_t pold;
   int inexact, sign;
   MPFR_SAVE_EXPO_DECL (expo);
 
+  /* Call the initialisation function of the cache if it's needed */
+  MPFR_DEFERRED_INIT_CALL(cache);
+
   MPFR_SAVE_EXPO_MARK (expo);
 
+  /* Get the cache in read only mode */
+  MPFR_LOCK_READ(cache->lock);
+  /* Read the precision within the cache */
+  pold = MPFR_PREC (cache->x);
   if (MPFR_UNLIKELY (prec > pold))
     {
-      /* No previous result in the cache or the precision of the previous
-         result is not sufficient. We increase the cache size by at least
-         10% to avoid invalidating the cache many times if one performs
-         several computations with small increase of precision. */
+      /* Free the cache in read only mode */
+      /* And get the cache in read write mode */
+      MPFR_LOCK_READ2WRITE(cache->lock);
 
-      if (MPFR_UNLIKELY (pold == 0))  /* No previous result. */
-        mpfr_init2 (cache->x, prec);  /* as pold = prec below */
-      else
-        pold += pold / 10;
+      /* Retest the precision once we get the lock.
+         If there is no lock, there is no harm in this code */
+      pold = MPFR_PREC (cache->x);
+      if (MPFR_LIKELY (prec > pold))
+        {
+          /* No previous result in the cache or the precision of the previous
+             result is not sufficient. We increase the cache size by at least
+             10% to avoid invalidating the cache many times if one performs
+             several computations with small increase of precision. */
+          if (MPFR_UNLIKELY (pold == 0))  /* No previous result. */
+            mpfr_init2 (cache->x, prec);  /* as pold = prec below */
+          else
+            pold += pold / 10;
 
-      if (pold < prec)
-        pold = prec;
+          /* Update the cache. */
+          if (pold < prec)
+            pold = prec;
 
-      /* no need to keep the previous value */
-      mpfr_set_prec (cache->x, pold);
-      cache->inexact = (*cache->func) (cache->x, MPFR_RNDN);
+          /* no need to keep the previous value */
+          mpfr_set_prec (cache->x, pold);
+          cache->inexact = (*cache->func) (cache->x, MPFR_RNDN);
+        }
+
+      /* Free the cache in read write mode */
+      /* Get the cache in read only mode */
+      MPFR_LOCK_WRITE2READ(cache->lock);
     }
 
+  /* now pold >= prec is the precision of cache->x */
   MPFR_ASSERTD (pold >= prec);
   MPFR_ASSERTD (MPFR_PREC (cache->x) == pold);
 
@@ -151,5 +182,9 @@ mpfr_cache (mpfr_ptr dest, mpfr_cache_t cache, mpfr_rnd_t rnd)
     }
 
   MPFR_SAVE_EXPO_FREE (expo);
+
+  /* Free the cache in read  mode */
+  MPFR_UNLOCK_READ(cache->lock);
+
   return mpfr_check_range (dest, inexact, rnd);
 }

commit 1e627dd7fe277e685a13824834ccfe4754e9f3ad

[src/mpfr-impl.h] Fixed bug introduced in r10327, giving: error: ISO C does not allow extra ';' outside of a function [-Wpedantic]

src/mpfr-impl.h
@@ -1136,7 +1136,7 @@ typedef union { mp_size_t s; mp_limb_t l; } mpfr_size_limb_t;
   MPFR_CACHE_ATTR mpfr_cache_t _cache = {{                           \
       {{ 0, MPFR_SIGN_POS, 0, (mp_limb_t *) 0 }}, 0, _func           \
       MPFR_DEFERRED_INIT_SLAVE_VALUE(_func)                          \
-    }};
+    }}
 
 /******************************************************
  ***************  Threshold parameters  ***************